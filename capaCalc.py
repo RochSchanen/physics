c513880883ef29771becb8f9cac6df6a5a0edb23
Schanen <schanen@lancaster.ac.uk>
2021-03-30 10:34:11 +0100

    add capaCalc.py

---
+++ capaCalc.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# file: capaCalc.py
+# content: capacitance calculator
+# created: 2021 March 30 Tuesday
+# modified:
+# modification:
+# author: roch schanen
+# comment:
+
+'''
+1) state definitions
+2) find numerical method
+3) implement
+'''
